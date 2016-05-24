3b2087d9df703c5f7a3358dc80082f20da9fe92e
rolivieri <roliv@us.ibm.com>
2016-05-24 16:51:01 -0500

    Updated Package.swift to reference latest Kitura version.

--- Package.swift
+++ Package.swift
@@ -25,7 +25,7 @@ let package = Package(
         )
     ],
     dependencies: [
-        .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 0, minor: 13),
+        .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 0, minor: 15),
         .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 0, minor: 7),
         .Package(url: "https://github.com/IBM-Swift/Swift-cfenv", majorVersion: 1, minor: 0)
     ],
