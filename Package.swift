a29c4551bf17e3ca862ae07e68d10c1f9c897218
Brian Hernandez <bhernand@us.ibm.com>
2016-07-22 10:00:21 -0500

    Migrated to Swift 06-20

--- Package.swift
+++ Package.swift
@@ -25,8 +25,8 @@ let package = Package(
         )
     ],
     dependencies: [
-        .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 0, minor: 19),
-        .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 0, minor: 10),
-        .Package(url: "https://github.com/IBM-Swift/Swift-cfenv", majorVersion: 1, minor: 3)
+        .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 0, minor: 22),
+        .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 0, minor: 12),
+        .Package(url: "https://github.com/IBM-Swift/Swift-cfenv", majorVersion: 1, minor: 4)
     ],
     exclude: ["Makefile", "Kitura-Build"])
