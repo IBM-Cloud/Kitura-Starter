d74917f724bf41edb6b315c904f1662b313e19a7
Brian Hernandez <bhernand@us.ibm.com>
2016-11-18 12:37:47 -0600

    Updated cf-deployment-tracker

--- Package.swift
+++ Package.swift
@@ -26,6 +26,6 @@ let package = Package(
       .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 1, minor: 1),
       .Package(url: "https://github.com/IBM-Swift/Swift-cfenv.git", majorVersion: 1, minor: 8),
       .Package(url: "https://github.com/IBM-Swift/SwiftyJSON.git", majorVersion: 15),
-      .Package(url: "https://github.com/IBM-Bluemix/cf-deployment-tracker-client-swift.git", majorVersion: 0, minor: 5)
+      .Package(url: "https://github.com/IBM-Bluemix/cf-deployment-tracker-client-swift.git", majorVersion: 0, minor: 6)
     ],
     exclude: ["Makefile", "Package-Builder"])
