bca730686410295d54349cf8e0773b543812ff79
Brian Hernandez <bhernand@us.ibm.com>
2016-12-05 12:14:22 -0600

    Kitura 1.3 update

--- Package.swift
+++ Package.swift
@@ -22,8 +22,8 @@ let package = Package(
       Target(name: "Kitura-Starter", dependencies: [])
     ],
     dependencies: [
-      .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 2),
-      .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 1, minor: 1),
+      .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 3),
+      .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 1, minor: 3),
       .Package(url: "https://github.com/IBM-Swift/Swift-cfenv.git", majorVersion: 1, minor: 8),
       .Package(url: "https://github.com/IBM-Swift/SwiftyJSON.git", majorVersion: 15),
       .Package(url: "https://github.com/IBM-Bluemix/cf-deployment-tracker-client-swift.git", majorVersion: 0, minor: 6)
