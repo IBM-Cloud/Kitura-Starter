02e4df239dbe47928e8fab6f9d9110419684e17d
Christian Compton <cbcompto@us.ibm.com>
2017-01-26 17:02:35 -0600

    updating versions

--- Package.swift
+++ Package.swift
@@ -22,7 +22,7 @@ let package = Package(
       Target(name: "Kitura-Starter", dependencies: [])
     ],
     dependencies: [
-      .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 5),
+      .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 6),
       .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 1),
       .Package(url: "https://github.com/IBM-Swift/Swift-cfenv.git", majorVersion: 2),
       .Package(url: "https://github.com/IBM-Bluemix/cf-deployment-tracker-client-swift.git", majorVersion: 1)
