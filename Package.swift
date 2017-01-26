f1ab037fbdfa52f501be9026868562f7cdce0a3a
Christian Compton <cbcompto@us.ibm.com>
2017-01-26 15:10:09 -0600

    Removing minor versions for some dependencies

--- Package.swift
+++ Package.swift
@@ -23,8 +23,8 @@ let package = Package(
     ],
     dependencies: [
       .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 5),
-      .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 1, minor: 5),
-      .Package(url: "https://github.com/IBM-Swift/Swift-cfenv.git", majorVersion: 2, minor: 0),
-      .Package(url: "https://github.com/IBM-Bluemix/cf-deployment-tracker-client-swift.git", majorVersion: 1, minor: 0)
+      .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 1),
+      .Package(url: "https://github.com/IBM-Swift/Swift-cfenv.git", majorVersion: 2),
+      .Package(url: "https://github.com/IBM-Bluemix/cf-deployment-tracker-client-swift.git", majorVersion: 1)
     ],
     exclude: ["Makefile", "Package-Builder"])
