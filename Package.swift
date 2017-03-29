7d25fe36cb74a0ace4d2ff799466b91ec4933c14
Christian Compton <cbcompto@us.ibm.com>
2017-03-29 15:45:11 -0500

    3.1 migration

--- Package.swift
+++ Package.swift
@@ -22,8 +22,8 @@ let package = Package(
     Target(name: "Kitura-Starter", dependencies: [])
   ],
   dependencies: [
-    .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 6),
-    .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 1, minor: 6),
+    .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 7),
+    .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 1, minor: 7),
     .Package(url: "https://github.com/IBM-Bluemix/cf-deployment-tracker-client-swift.git", majorVersion: 3)
   ]
 )
