a8de5c525e9663dcc50ccf92561b2c7ab774336f
rolivieri <roliv@us.ibm.com>
2017-03-28 21:15:41 -0500

    Updated package.swift as a work-around for production issue.

--- Package.swift
+++ Package.swift
@@ -23,7 +23,7 @@ let package = Package(
   ],
   dependencies: [
     .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 6),
-    .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 1),
+    .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 1, minor: 6),
     .Package(url: "https://github.com/IBM-Bluemix/cf-deployment-tracker-client-swift.git", majorVersion: 2)
   ]
 )
