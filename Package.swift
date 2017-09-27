3b6af9fd34c5fddd68fbac6ad1c9b7fdaabb0871
Aaron Liberatore <aaron.m.liberatore@ibm.com@aarons-mbp.austin.ibm.com>
2017-09-27 14:38:25 -0500

    swift 4

--- Package.swift
+++ Package.swift
@@ -1,33 +1,47 @@
+// swift-tools-version:4.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 /**
-* Copyright IBM Corporation 2016, 2017
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-* http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-**/
+ * Copyright IBM Corporation 2016, 2017
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
 
 import PackageDescription
 
 let package = Package(
-  name: "Kitura-Starter",
-  targets: [
-    Target(name: "Kitura-Starter",
-      dependencies: [.Target(name: "Controller")]),
-    Target(name: "Controller")
-  ],
-  dependencies: [
-    .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 7),
-    .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 1, minor: 7),
-    .Package(url: "https://github.com/IBM-Swift/CloudEnvironment.git", majorVersion: 4),
-    .Package(url: "https://github.com/IBM-Bluemix/cf-deployment-tracker-client-swift.git", majorVersion: 4),
-    .Package(url: "https://github.com/IBM-Swift/Health.git", majorVersion: 0)
-  ]
+    name: "Kitura-Starter",
+    products: [
+      .library(
+        name: "Kitura-Starter",
+        targets: ["Kitura-Starter"]
+      )
+    ],
+    dependencies: [
+      .package(url: "https://github.com/IBM-Swift/Kitura.git", .upToNextMinor(from: "1.7.0")),
+      .package(url: "https://github.com/IBM-Swift/HeliumLogger.git", .upToNextMinor(from: "1.7.0"))
+      .package(url: "https://github.com/IBM-Swift/CloudEnvironment.git", .upToNextMajor(from: "5.0.0")),
+      .package(url: "https://github.com/IBM-Bluemix/cf-deployment-tracker-client-swift.git", .upToNextMajor(from: "5.0.0")),
+      .package(url: "https://github.com/IBM-Swift/Health.git", .upToNextMajor(from: "0.0.0"))
+    ],
+    targets: [
+      .target(
+        name: "Kitura-Starter",
+        dependencies: ["Kitura", "HeliumLogger", "CloudEnvironment", "CloudFoundryDeploymentTracker", "Health"]
+      ),
+      .testTarget(
+        name: "ControllerTests",
+        dependencies: ["Kitura-Starter"]
+      )
+    ]
 )
